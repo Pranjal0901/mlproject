44a95ef586e1667c6a7cb9a0d30145ab34687881
Pranjal Singh <pranjalsinghtdl@gmail.com>
2025-10-17 22:27:19 +0530

    ML Project

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -26,7 +26,7 @@ class DataIngestion:
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            logging.info("Train Test slit initiated")
+            logging.info("Train Test split initiated")
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
@@ -38,5 +38,10 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
 
-        except:
-            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
+            
