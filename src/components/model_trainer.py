d7d667f1805227b769280b37a652704a39d24c90
Pranjal Singh <pranjalsinghtdl@gmail.com>
2025-10-19 00:09:08 +0530

    ML Project

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,10 +43,41 @@ class ModelTrainer:
                 "K-Neighbours Regression": KNeighborsRegressor(),
                 "XGBClassifier": XGBRegressor(),
                 "Catboost Classifier":CatBoostRegressor(),
-                "Adaboost Classifier":AdaBoostRegressor()
+                "AdaBoost Classifier":AdaBoostRegressor()
+            }
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbours Regression":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBClassifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Catboost Classifier":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
             }
 
-            model_report:dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            model_report:dict = evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                               models=models,params=params)
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -18,13 +19,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,params):
     try:
         report={}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(x_train, y_train)
+            para = params[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(x_train,y_train)
+            
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
             train_model_score = r2_score(y_train,y_train_pred)
